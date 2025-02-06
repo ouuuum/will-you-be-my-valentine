d8040e6688e359b78e719e90b375c0bc5f84b461
ouuuum <oumaima_aderdour@outlook.com>
2025-02-06 23:10:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Good girl!!! I LOVE YOUUU:3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "BAKAAAA?",
+    "HOW DARE YOU!!!,
+    "Press yes :) ",
+    "YES!!! :) ",
+  
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
